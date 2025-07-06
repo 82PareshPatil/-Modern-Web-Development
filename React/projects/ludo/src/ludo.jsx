0d542fef2e32dbc255d1c24dda89a1a8f5107a13
Paresh Patil <paresh2004patil@gmail.com>
2025-07-06 23:08:26 +0530

    new file added

--- React/projects/ludo/src/ludo.jsx
+++ React/projects/ludo/src/ludo.jsx
@@ -2,19 +2,26 @@ import { useState } from "react";
 
 function Ludo() {
   const [move, setMove] = useState({ blue: 0, red: 0, ye: 0, gr: 0 });
-
+  const [ch,setch] = useState(["no move"])
   const change = (color) => {
     setMove((prevMove) => ({
       ...prevMove,
       [color]: prevMove[color] + 1,
+
     }));
+    setch((newCh) => {
+  return [...newCh, "next move"];
+});
+
   };
+
 
   return (
     <div>
       <p>Welcome! to Ludo board</p>
       <div>
         <p>Blue: {move.blue}</p>
+        <p>{ch}</p>
         <button onClick={() => change("blue")}>Blue +1</button>
 
         <p>Red: {move.red}</p>
