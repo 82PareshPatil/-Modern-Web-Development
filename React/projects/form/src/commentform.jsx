856022cf2aeedb667ece0c0c9d09052d7402d34f
Paresh Patil <paresh2004patil@gmail.com>
2025-07-12 16:44:48 +0530

    new comment file

--- React/projects/form/src/commentform.jsx
+++ React/projects/form/src/commentform.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react"
+
 export default function Commentform(){
+    let [formdata,setformdata]=useState({
+        username : "",
+        remark : "",
+        rating : ""
+    })
     return(
         <form >
             <label htmlFor="username">Username : </label>
             <input type="text" 
             name="username" 
-            id="username" />
+            id="username" 
+            value={formdata.username}
+            />
             <br />
 
             <label htmlFor="username">Remark : </label>
             <input type="text" 
             name="remark" 
-            id="remark" />
+            id="remark" 
+            value={formdata.remark}
+            />
             <br />
 
             <label htmlFor="username">Rating : </label>
@@ -18,7 +29,9 @@ export default function Commentform(){
             name="rating" 
             id="rating" 
             min={1}
-            max={5}/>
+            max={5}
+            value={formdata.rating}
+            />
             <br /><br />
             <button>Submit</button>
         </form>
