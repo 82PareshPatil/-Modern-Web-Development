fca7578ac57e6df6e4ed30ecaeb84ec41527c8fa
Paresh Patil <paresh2004patil@gmail.com>
2025-07-12 10:44:40 +0530

    new files

--- React/projects/form/src/form1.jsx
+++ React/projects/form/src/form1.jsx
@@ -1,8 +1,49 @@
+import { useState } from "react"
+
 export default function Form1(){
+    let [formdata,setformdata]=useState({
+        username : "",
+        password : ""
+    })
+
+   let datainsert=(e)=>{
+       setformdata((cur)=>{
+       return {...cur,[e.target.name]:e.target.value}
+       })
+   }
+   let formsubmit=(e)=>{
+       e.preventDefault();
+       console.log(formdata);
+       setformdata({
+        username : "",
+        password : ""
+    })
+   }
+
     return(
-        <div>
-            <p>hi</p>
-        </div>
+        <form onSubmit={formsubmit}>
+            <label htmlFor="uname">User Name : </label>
+            <input type="text" 
+            placeholder="Enter Username"
+            id="uname"
+            name="username"
+            value={formdata.username}
+            onChange={datainsert}
+             autoComplete="username" 
+            />
+            <br />
+            <label htmlFor="pass">Password : </label>
+            <input type="password" 
+            name="password" 
+            id="pass" 
+            placeholder="****"
+            value={formdata.password}
+            onChange={datainsert}
+             autoComplete="current-password" 
+            /><br /><br />
+            <button>Submit</button>
+
+        </form>
     )
 }
 export {Form1}
