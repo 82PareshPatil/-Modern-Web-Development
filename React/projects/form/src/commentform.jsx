eec457841c3b8507930a26828d4fe1636f260f5b
Paresh Patil <paresh2004patil@gmail.com>
2025-07-12 16:40:17 +0530

    new comment file

--- React/projects/form/src/commentform.jsx
+++ React/projects/form/src/commentform.jsx
@@ -1,7 +1,26 @@
 export default function Commentform(){
     return(
         <form >
-            <input type="text" name="" id="" />
+            <label htmlFor="username">Username : </label>
+            <input type="text" 
+            name="username" 
+            id="username" />
+            <br />
+
+            <label htmlFor="username">Remark : </label>
+            <input type="text" 
+            name="remark" 
+            id="remark" />
+            <br />
+
+            <label htmlFor="username">Rating : </label>
+            <input type="number" 
+            name="rating" 
+            id="rating" 
+            min={1}
+            max={5}/>
+            <br /><br />
+            <button>Submit</button>
         </form>
     )
 }
