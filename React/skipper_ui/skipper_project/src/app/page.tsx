fdd8b81268eeb0708a0a7223a63bc7ec45abfce3
82PareshPatil <jay2001p@gmail.com>
2025-08-02 19:42:00 +0530

    new

--- React/skipper_ui/skipper_project/src/app/page.tsx
+++ React/skipper_ui/skipper_project/src/app/page.tsx
@@ -1,8 +1,22 @@
+'use client'
+import ShareButton from '@/components/ui/share-button'
+import WrapButton from '@/components/ui/wrap-button'
+import { Instagram, Linkedin, Twitter } from 'lucide-react'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <h1 className='text-white'>Hello , My Name is Paresh!
+          <WrapButton >
+            Get started
+        </WrapButton>
+        <ShareButton links={
+          [{icon:Twitter , href:"https://x.com/patil_pare82588"},{icon:Linkedin,href:"https://www.linkedin.com/in/paresh-patil-171801248/"},{icon:Instagram, href:"https://www.instagram.com/81_paresh/"}]
+        }>Online</ShareButton>
+      </h1>
+    </div>
+    
   )
 }
 
