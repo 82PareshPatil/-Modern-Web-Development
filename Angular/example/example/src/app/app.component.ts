a3f63a405876cc5b5ccde98bcae16971cf6b7855
82PareshPatil <paresh2004patil@gmail.com>
2025-08-07 17:13:29 +0530

    switch case

--- Angular/example/example/src/app/app.component.ts
+++ Angular/example/example/src/app/app.component.ts
@@ -1,43 +1,69 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { TemplateVariableComponent } from './template-variable/template-variable.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LoginComponent,TemplateVariableComponent],
+  imports: [LoginComponent, TemplateVariableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
-  // styles:['h1{background-color:red}']
 })
 export class AppComponent {
   title = 'example';
-  display=''
-  na=''
-  event(e:MouseEvent){
-    console.log("click!",e.type)
+  display = '';
+  na = '';
+
+  event(e: MouseEvent) {
+    console.log("click!", e.type)
     console.log(e.target)
     console.log((e.target as Element).className)
     console.log((e.target as HTMLButtonElement).name);
   }
-  another(e:any){
-    this.na=(e.target as HTMLInputElement).value;
+
+  another(e: Event) {
+    this.na = (e.target as HTMLInputElement).value;
+  }
+
+  showname() {
+    this.display = this.na;
+  }
+
+  setname() {
+    this.na = "Paresh";
+  }
+
+  displayname: boolean = true;
+  toggle() {
+    this.displayname = !this.displayname;
+  }
+
+  toglediv2 = true;
+  newtogle() {
+    this.toglediv2 = !this.toglediv2;
+  }
+
+  color1 = 1;
+  chnagecl(val: number) {
+    if (val >= 1 && val <= 4) {
+      this.color1 = val;
+    } else {
+      this.color1 = 5;
+    }
+  }
+
+  // ✅ FIXED: Now this updates the color based on typed value
+  colorchange(e: Event) {
+    const numberval = parseInt((e.target as HTMLInputElement).value);
+    this.chnagecl(numberval);
+  }
  
+  cs="yellow"
+  swchange(val:string){
+    this.cs=val;
   }
-  showname(){
-     this.display=this.na;
-  }
-  setname(){
-    this.na="Paresh"
-  }  
-  toglediv2=true;
-  displayname:boolean=true;
-   toggle(){
-    this.displayname=!this.displayname
-   }
-   newtogle(){
-    this.toglediv2=!this.toglediv2;
-   }
+  newcolor(e: Event){
+       this.cs=(e.target as HTMLInputElement).value;
+    }
 
 }
