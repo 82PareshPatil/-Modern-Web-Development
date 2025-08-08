d0182ac427cf0219216d80938ce3dfa670cfea9a
82PareshPatil <paresh2004patil@gmail.com>
2025-08-08 17:14:05 +0530

    new file add

--- Angular/example/angular_new/src/app/app.routes.ts
+++ Angular/example/angular_new/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { LogoutComponent } from './logout/logout.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"login",component:LoginComponent},
+    {path:"logout",component:LogoutComponent},
+    {path:"About",component:AboutComponent}
+];
