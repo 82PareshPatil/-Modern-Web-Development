e161f3d6c4e608a3862117cdb608a905768bcd35
82PareshPatil <paresh2004patil@gmail.com>
2025-08-08 14:45:56 +0530

    new file of to_do_list

--- Angular/example/to_do_list/src/app/app.component.ts
+++ Angular/example/to_do_list/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'to_do_list';
+  task = ""
+  todo:{id:number,task:string}[]=[]
+  addtask(){
+     this.todo.push({id:this.todo.length+1,task:this.task})
+     this.task="";
+  }
+  delet(tl:number){
+    this.todo=this.todo.filter((item)=>item.id!=tl)
+  }
 }
