eae0b965fb6cf4e8f60a50d246a6889ec9a5f1e0
82PareshPatil <paresh2004patil@gmail.com>
2025-08-11 09:39:45 +0530

    some exisiting files

--- Angular/example/angular_new/src/app/app.routes.ts
+++ Angular/example/angular_new/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { LogoutComponent } from './logout/logout.component';
 import { LoginComponent } from './login/login.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
     {path:"login",component:LoginComponent},
     {path:"logout",component:LogoutComponent},
-    {path:"About",component:AboutComponent}
+    {path:"About",component:AboutComponent},
+    {path:"**",component:NotfoundComponent}
 ];
